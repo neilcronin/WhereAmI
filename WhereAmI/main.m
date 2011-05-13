d826b50402c39fbc9951984b6c73d619313bba16
Neil Cronin <neil@rackle.com>
2011-05-13 13:35:50 -0700

    Initial Commit

---
+++ WhereAmI/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WhereAmI
+//
+//  Created by Neil Cronin on 5/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+

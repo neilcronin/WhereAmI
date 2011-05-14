735753658a0902d139d42d44f30bb185ab4278a3
Neil Cronin <neil@rackle.com>
2011-05-14 12:25:47 -0700

    add license.

--- WhereAmI/WhereAmI.m
+++ WhereAmI/WhereAmI.m
@@ -1,10 +1,19 @@
 //
-//  WhereAmI.m
-//  WhereAmI
+// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+// Version 2, December 2004
 //
-//  Created by Neil Cronin on 5/13/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+// Created by Neil Cronin <neil@rackle.com> on 5/13/11.
 //
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+// TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+// 0. You just DO WHAT THE FUCK YOU WANT TO.
+//
+
 
 #import "WhereAmI.h"
 
